cae962cf6e94e614bb58bbadcc85ffd916356025
BrandonAbe <28733924+BrandonAbe@users.noreply.github.com>
2025-05-10 21:49:24 -0400

    Add print of TextNode type to see what it looks like when printed

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-print("Hello, world!")
+from textnode import TextNode
+from textnode import TextType
+
+# Main function definition
+def main():
+    text_node = TextNode("This is some anchor text",TextType.LINK,"https://www.boot.dev")
+    print(text_node)
+
+# Start main() loop
+main()
