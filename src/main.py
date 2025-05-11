24f17ae5fc4f2e296c9e1149f2e847a240d8338b
BrandonAbe <28733924+BrandonAbe@users.noreply.github.com>
2025-05-11 02:46:33 -0400

    Create html_node in main

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 from textnode import TextNode
 from textnode import TextType
+from htmlnode import HTMLNode
 
 # Main function definition
 def main():
-    text_node = TextNode("This is some anchor text",TextType.LINK,"https://www.boot.dev")
-    print(text_node)
+    text_node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+    html_node = HTMLNode("div", "Hello, Boot.dev!", None, {"class": "greeting", "href": "https://boot.dev"},)
 
 # Start main() loop
 main()
