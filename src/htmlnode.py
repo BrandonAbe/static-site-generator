c412e4c106ff91cafea98dfe480101954a7d1109
BrandonAbe <28733924+BrandonAbe@users.noreply.github.com>
2025-05-11 21:14:23 -0400

    Update ParentNode.to_html() and ParentNode.__repr__()

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -50,15 +50,18 @@ class LeafNode(HTMLNode):
 
 
 class ParentNode(HTMLNode):
-    def __init__(self, tag:str, children:list, props:dict=None):
+    def __init__(self, tag, children, props=None):
         super().__init__(tag, None, children, props)
 
     def to_html(self):
         if self.tag is None:
-            raise ValueError("Invalid HTML: no tag")
+            raise ValueError("invalid HTML: no tag")
         if self.children is None:
-            raise ValueError("Invalid HTML: no children")
+            raise ValueError("invalid HTML: no children")
         children_html = ""
         for child in self.children:
-            children_html += child.to_html() #Recursive call
-        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}"
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
