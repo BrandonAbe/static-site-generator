9b94e9565adfbf86906bc2fdc8146113bebe4e48
BrandonAbe <28733924+BrandonAbe@users.noreply.github.com>
2025-05-11 20:49:09 -0400

    Add ParentNode class, which inherits from HTMLNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -46,3 +46,19 @@ class LeafNode(HTMLNode):
     
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
+    
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag:str, children:list, props:dict=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Invalid HTML: no tag")
+        if self.children is None:
+            raise ValueError("Invalid HTML: no children")
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html() #Recursive call
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}"
