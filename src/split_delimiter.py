0a0acac918d03692ea55111f69cb9d0edae316ac
BrandonAbe <28733924+BrandonAbe@users.noreply.github.com>
2025-05-18 23:19:33 -0400

    refactor new_list -> new_node; Add logic for checking contents of each section. Add append to new_node.

--- src/split_delimiter.py
+++ src/split_delimiter.py
@@ -2,13 +2,13 @@ from typing import Annotated
 from textnode import TextType, TextNode
 
 def split_nodes_delimiter(old_nodes: list, delimiter: Annotated[str, "single character"], text_type: TextType):
-    new_list = []
+    new_node = []
     for node in old_nodes:
         if delimiter not in node.text:
             raise Exception("Invalid Markdown: missing delimiter")
         # Keep unnchanged if not a TextNode
         if node.text_type != TextType.TEXT:
-            new_list.append(node)
+            new_node.append(node)
             continue
 
         split_nodes = []
@@ -24,7 +24,12 @@ def split_nodes_delimiter(old_nodes: list, delimiter: Annotated[str, "single cha
         if len(sections) % 2 == 0:
             raise Exception("Invalid Markdown: missing closing delimiters")
         
-
-
-
-    return new_list    
+        for i in range(len(sections)):
+            if sections[i] == "":   # If content is empty string, do nothing
+                continue
+            if i % 2 == 0:
+                split_nodes.append(TextNode(sections[i], TextType.TEXT)) # If even index, content is enum
+            else:
+                split_nodes.append(TextNode(sections[i], text_type)) # If odd index, content is text_type
+        new_node.extend(split_nodes)
+    return new_node   
