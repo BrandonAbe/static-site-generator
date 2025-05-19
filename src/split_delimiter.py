d924597f8d924730157530f787fc073df185f28c
BrandonAbe <28733924+BrandonAbe@users.noreply.github.com>
2025-05-18 22:25:11 -0400

    Update imports; fixed logic of delimiter check and isinstance()

--- src/split_delimiter.py
+++ src/split_delimiter.py
@@ -1,13 +1,11 @@
 from typing import Annotated
-from textnode import TextType
+from textnode import TextType, TextNode
 
 def split_nodes_delimiter(old_nodes: list, delimiter: Annotated[str, "single character"], text_type: TextType):
     new_list = []
     for node in old_nodes:
-        if delimiter not in node:
+        if delimiter not in node.text:
             raise Exception("Invalid Markdown: missing delimiter")
-        if not isinstance(node, TextType.TEXT):
+        if isinstance(node, TextNode):
             new_list.append(node)
     return new_list    
-    
-        
