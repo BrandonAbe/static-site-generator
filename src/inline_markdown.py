9bd3dfa5d67a18ed3a4710a549134ee8becbe743
BrandonAbe <28733924+BrandonAbe@users.noreply.github.com>
2025-05-20 19:35:59 -0400

    Added forgotten return statement to extract_markdown_links()

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -51,3 +51,4 @@ def extract_markdown_images(text: str):
 def extract_markdown_links(text: str):
     pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
     matches = re.findall(pattern,text)
+    return matches
