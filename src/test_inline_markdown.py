b6c33fb0cf2672d1951d1d976d99af67139f1b38
BrandonAbe <28733924+BrandonAbe@users.noreply.github.com>
2025-05-21 22:21:46 -0400

    Add unit test for split_nodes_link

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,5 +1,11 @@
 import unittest
-from inline_markdown import *
+from inline_markdown import (
+    split_nodes_delimiter,
+    split_nodes_image,
+    split_nodes_link,
+    extract_markdown_links,
+    extract_markdown_images,
+)
 from textnode import TextNode, TextType
 
 class TestInlineMarkdown(unittest.TestCase):
@@ -114,6 +120,22 @@ class TestInlineMarkdown(unittest.TestCase):
                 TextNode(" and another ", TextType.TEXT),TextNode("second image", 
                 TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"),
             ],new_nodes,)
+
+    def test_split_links(self):
+        node = TextNode(
+            "This is text with a [link](https://boot.dev) and [another link](https://blog.boot.dev) with text that follows",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+                TextNode(" and ", TextType.TEXT),
+                TextNode("another link", TextType.LINK, "https://blog.boot.dev"),
+                TextNode(" with text that follows", TextType.TEXT),
+            ],new_nodes,)
+
 
 
 if __name__ == "__main__":
