ed84f23716c95c44bae43810fbceaaafc8fcc879
BrandonAbe <28733924+BrandonAbe@users.noreply.github.com>
2025-05-21 21:38:56 -0400

    Add 2 additional unit tests for split_nodes_image

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -96,6 +96,25 @@ class TestInlineMarkdown(unittest.TestCase):
         new_nodes = split_nodes_image([node])
         self.assertListEqual([TextNode("This is text w/ an ", TextType.TEXT),TextNode("image", TextType.IMAGE, "https://www.google.com")],new_nodes)
 
+    def test_split_image_single(self):
+        node = TextNode("![image](https://www.example.COM/IMAGE.PNG)",TextType.TEXT,)
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual([TextNode("image", TextType.IMAGE, "https://www.example.COM/IMAGE.PNG"),],new_nodes,)
+
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),TextNode("second image", 
+                TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"),
+            ],new_nodes,)
+
 
 if __name__ == "__main__":
     unittest.main()
