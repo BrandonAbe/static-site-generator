87ec4f39c4a2de5d90f70e0cfb3351eadd3d9756
BrandonAbe <28733924+BrandonAbe@users.noreply.github.com>
2025-05-23 19:39:02 -0400

    Add enumeration for BlockType. Add block_to_block_type which determines the enum of the BlockType object.

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,13 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    OLIST = "ordered_list"
+    ULIST = "unordered_list"
+
 def markdown_to_blocks(markdown):
     split_blocks = []
     blocks = markdown.split("\n\n")
@@ -6,3 +16,29 @@ def markdown_to_blocks(markdown):
         if cleaned_block != "":
             split_blocks.append(cleaned_block)
     return split_blocks
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+    
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")): # .startswith can take string or tuple. I use a tuple to check up to Heading 6
+        return BlockType.HEADING
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"): # If open and closed with ```, then it is a code block. If length of lines is <=1, it's not a block.
+        return BlockType.CODE
+    if block.startswith(">"): # Check if quote
+        for line in lines:
+            if not line.startswith(">"):
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+    if block.startswith("- "):# Check if unordered list
+        for line in lines:
+            if not line.startswith("- "):
+                return BlockType.PARAGRAPH
+        return BlockType.ULIST
+    if block.startswith("1. "): # Check if ordered list. Assumtion: All ordered lists will start with 1
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "): # If line is not any other enum, it is a paragraph
+                return BlockType.PARAGRAPH
+            i += 1
+        return BlockType.OLIST
+    return BlockType.PARAGRAPH
