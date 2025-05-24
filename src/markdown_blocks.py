dd60099441182ff0f7eb1ab48b150a3e3fc5ed79
BrandonAbe <28733924+BrandonAbe@users.noreply.github.com>
2025-05-24 12:04:24 -0400

    Add markdown_to_html_node checks for PARAGRAPH, HEADING, CODE

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,4 +1,12 @@
 from enum import Enum
+from htmlnode import (
+    ParentNode,
+    LeafNode,
+    )
+from textnode import (
+    text_node_to_html_node,
+)
+from inline_markdown import text_to_textnodes
 
 class BlockType(Enum):
     PARAGRAPH = "paragraph"
@@ -42,3 +50,55 @@ def block_to_block_type(block):
             i += 1
         return BlockType.OLIST
     return BlockType.PARAGRAPH
+
+# Helper function
+def text_to_children(text):
+    text_nodes = text_to_textnodes(text) #splits text into each node type based on content
+    children_html_nodes = []
+    for text_node in text_nodes:
+        children_html_nodes.append(text_node_to_html_node(text_node))
+    return children_html_nodes
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    html_node = None #Default
+
+    if not blocks: # Return empty or whitespace input to return as empty div using LeafNode as ParentNode requires children
+        return LeafNode("div", "")
+
+    for block in blocks:
+        block_type = block_to_block_type(block)
+
+        if block_type == BlockType.PARAGRAPH:
+            children = text_to_children(block)
+            html_node = ParentNode("p",children)
+
+        elif block_type == BlockType.HEADING: 
+            heading_level = 0
+            for char in block:
+                if char == "#":
+                    heading_level += 1
+                else:
+                    break
+            heading_text = block[heading_level:].strip()
+            children = text_to_children(heading_text)
+            html_node = ParentNode(f"h{heading_level}",children)
+
+        elif block_type == BlockType.CODE:
+            code_content = block[3:-3]
+            if code_content.startswith("\n"): # Handle if code content has new line at the start (edge case)
+                code_content = code_content[1:]
+            raw_code_leaf = LeafNode(None,code_content) # Assignment CH4_L3 requirement: "code" block shall not do any inline markdown parsing of it's children
+            code_html = ParentNode("code",[raw_code_leaf])
+            html_node = ParentNode("pre",[code_html]) # "pre" tag comes from CH4_L3 tip: "Code blocks should be surrounded by a <code> tag nested inside a <pre> tag."
+
+        elif block_type == BlockType.QUOTE:
+            pass
+
+        elif block_type == BlockType.OLIST:
+            pass
+
+        elif block_type == BlockType.ULIST:
+            pass
+        else:
+            raise ValueError(f"Unknown block type: {block_type}")
