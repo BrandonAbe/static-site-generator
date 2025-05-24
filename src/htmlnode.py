e0b2183aa2788f14105e8780d2205b78e5a09c47
BrandonAbe <28733924+BrandonAbe@users.noreply.github.com>
2025-05-24 11:54:38 -0400

    Add type hinting for ParentNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -50,7 +50,7 @@ class LeafNode(HTMLNode):
 
 
 class ParentNode(HTMLNode):
-    def __init__(self, tag, children, props=None):
+    def __init__(self, tag:str, children:list, props=None):
         super().__init__(tag, None, children, props)
 
     def to_html(self):
