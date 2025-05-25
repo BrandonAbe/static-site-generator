fcadd7e1a245dc464770510aee81ea965029b32e
BrandonAbe <28733924+BrandonAbe@users.noreply.github.com>
2025-05-25 13:27:36 -0400

    Added HTML Generation from dir_path_content to dir_path_public, using template file

--- src/main.py
+++ src/main.py
@@ -2,10 +2,13 @@
 import os
 import shutil
 from markdown_blocks import copy_files_recursive
+from generate_content import generate_page
 
 # Variable definitions
 dir_path_static = "./static"
 dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 # Main function definition
 def main():
@@ -16,6 +19,11 @@ def main():
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
 
+    print("Generating HTML...")
+    generate_page(
+        os.path.join(dir_path_content, "index.md"),
+        template_path,
+        os.path.join(dir_path_public, "index.html"),)
 
 # Start main() loop
 main()
