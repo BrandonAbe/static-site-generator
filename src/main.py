4a70e67bf8cc8f00e64385fe9ca1eacd65195722
BrandonAbe <28733924+BrandonAbe@users.noreply.github.com>
2025-05-24 23:31:03 -0400

    Refactor main() to delete public directories per requirement. Recursively copy static files to public files

--- src/main.py
+++ src/main.py
@@ -1,11 +1,21 @@
-from textnode import TextNode
-from textnode import TextType
-from htmlnode import HTMLNode
+# Imports
+import os
+import shutil
+from markdown_blocks import copy_files_recursive
+
+# Variable definitions
+dir_path_static = "./static"
+dir_path_public = "./public"
 
 # Main function definition
 def main():
-    text_node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    html_node = HTMLNode("div", "Hello, Boot.dev!", None, {"class": "greeting", "href": "https://boot.dev"},)
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
+
 
 # Start main() loop
 main()
