a66532495b5667a0bed99e5ed8f0ab5ce5e9dd7f
BrandonAbe <28733924+BrandonAbe@users.noreply.github.com>
2025-05-25 13:50:14 -0400

    Bug Fix: Corrected TextType.IMAGE return for src and alt text

--- src/textnode.py
+++ src/textnode.py
@@ -41,5 +41,4 @@ def text_node_to_html_node(text_node: TextNode):
                 src, alt_text = text_node.text.split('|',1)
                 return LeafNode("img", "", {"src": src, "alt": alt_text})
             else:
-                return LeafNode("img", "", {"src": text_node.text, "alt": ""})
-            #return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+                return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
