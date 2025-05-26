363ff1b666a7c1997967d0b8877b2a4e3b59d0e9
BrandonAbe <28733924+BrandonAbe@users.noreply.github.com>
2025-05-26 11:23:08 -0400

    Refactor html generation to generalized recursive content generation

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@
 import os
 import shutil
 from markdown_blocks import copy_files_recursive
-from generate_content import generate_page
+from generate_content import generate_pages_recursive
 
 # Variable definitions
 dir_path_static = "./static"
@@ -19,11 +19,8 @@ def main():
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
 
-    print("Generating HTML...")
-    generate_page(
-        os.path.join(dir_path_content, "index.md"),
-        template_path,
-        os.path.join(dir_path_public, "index.html"),)
+    print("Generating content...")
+    generate_pages_recursive(dir_path_content, template_path,dir_path_public)
 
 # Start main() loop
 main()
